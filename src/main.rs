1e76cf35d0808c9f74223e9fc48cf6ddbc6b1bad
Chris Zehner <cbzehner@gmail.com>
2020-08-21 20:35:10 -0700

    feat: hash passwords and store digest

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,17 @@ mod tests;
 
 use std::collections::{HashMap, HashSet};
 use std::convert;
+use std::convert::TryFrom;
 use std::sync::Mutex;
 
+use rocket::http::RawStr;
 use rocket::http::{Cookie, Cookies};
-use rocket::request::{self, FlashMessage, Form, FromRequest, Request};
+use rocket::outcome::IntoOutcome;
+use rocket::request::{self, FlashMessage, Form, FromFormValue, FromRequest, Request};
 use rocket::response::{Flash, Redirect};
-use rocket::{outcome::IntoOutcome, State};
+use rocket::State;
 use rocket_contrib::templates::Template;
+use scrypt::{scrypt_check, scrypt_simple, ScryptParams};
 use serde::Serialize;
 
 struct AppState {
@@ -23,18 +27,102 @@ struct AppState {
 }
 
 #[derive(FromForm)]
-struct Login {
+struct Login<'r> {
     username: String,
-    password: String,
+    password: Password<'r>,
 }
 
 #[derive(FromForm)]
-struct Registration {
+struct Registration<'r> {
     username: String,
     email: String,
     #[form(field = "preferred-name")]
     preferred_name: String,
-    password: String, // TODO: Reject weak passwords with https://github.com/magiclen/passwords or https://github.com/shssoichiro/zxcvbn-rs
+    password: Password<'r>,
+}
+
+#[derive(Copy, Clone)]
+struct Password<'v>(&'v str);
+
+impl<'v> std::fmt::Debug for Password<'v> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Password {{..}}")
+    }
+}
+
+impl<'v> std::fmt::Display for Password<'v> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Password {{..}}")
+    }
+}
+
+impl<'v> std::ops::Deref for Password<'v> {
+    type Target = &'v str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'v> std::ops::DerefMut for Password<'v> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<'v> FromFormValue<'v> for Password<'v> {
+    type Error = &'static str;
+
+    fn from_form_value(v: &'v RawStr) -> Result<Self, Self::Error> {
+        // TODO: Validate password strength (see examples/form_validation)
+        // Reject weak passwords with https://github.com/magiclen/passwords or https://github.com/shssoichiro/zxcvbn-rs
+        Ok(Password(v.as_str()))
+    }
+}
+
+impl<'a> Password<'a> {
+    fn digest(&mut self) -> Option<String> {
+        let raw_password = self.0;
+
+        if raw_password.is_empty() {
+            return None;
+        }
+
+        // TODO: Investigate whether there is a clever way to implement this.
+        // Allow a password to be read once-and-only-once by replacing it with an empty string.
+        self.0 = "";
+        Some(self.securely_hash(raw_password))
+    }
+
+    fn securely_hash(self, password: &str) -> String {
+        let params = ScryptParams::new(15, 8, 1).unwrap();
+        // TODO (post-rocket 0.5): Provide the ROCKET_SECRET as a salt to scrypt.
+        scrypt_simple(password, &params).expect("OS RNG should not fail")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Password;
+
+    #[test]
+    fn test_opaque_debug() {
+        let password = Password("NeverLeakAPassword");
+        assert_eq!(format!("{:?}", password), "Password {..}");
+    }
+
+    #[test]
+    fn test_opaque_display() {
+        let password = Password("NeverLeakAPassword");
+        assert_eq!(format!("{}", password), "Password {..}");
+    }
+
+    #[test]
+    fn test_digest_once() {
+        let mut password = Password("NeverLeakAPassword");
+        assert_eq!(password.digest().is_some(), true);
+        assert_eq!(password.digest().is_none(), true);
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize)]
@@ -43,7 +131,7 @@ struct User {
     username: String,
     email: String,
     preferred_name: String,
-    password: String, // TODO: Change to password digest and implement hashing
+    password_digest: String,
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for User {
@@ -62,6 +150,26 @@ impl<'a, 'r> FromRequest<'a, 'r> for User {
     }
 }
 
+impl std::convert::TryFrom<&mut Registration<'_>> for User {
+    type Error = &'static str;
+
+    fn try_from(registration: &mut Registration) -> Result<Self, Self::Error> {
+        match registration.password.digest() {
+            Some(password_digest) => {
+                let user_id = uuid::Uuid::new_v4();
+                Ok(User {
+                    id: user_id,
+                    username: registration.username.clone(),
+                    email: registration.email.clone(),
+                    preferred_name: registration.preferred_name.clone(),
+                    password_digest,
+                })
+            }
+            None => Err("Password from registration request has been consumed."),
+        }
+    }
+}
+
 impl User {
     fn lookup_user_by_id(id: uuid::Uuid, app_state: &AppState) -> Option<User> {
         match app_state.users.lock() {
@@ -73,16 +181,9 @@ impl User {
         }
     }
 
-    fn lookup_user_by_credentials(
-        username: String,
-        password: String,
-        app_state: State<AppState>,
-    ) -> Option<User> {
+    fn lookup_user_by_credentials(username: String, app_state: State<AppState>) -> Option<User> {
         match app_state.users.lock() {
-            Ok(users) => match users
-                .iter()
-                .find(|user| user.username == username && user.password == password)
-            {
+            Ok(users) => match users.iter().find(|user| user.username == username) {
                 Some(user) => Some((*user).clone()),
                 None => None,
             },
@@ -97,19 +198,16 @@ fn login(
     login: Form<Login>,
     app_state: State<AppState>,
 ) -> Result<Redirect, Flash<Redirect>> {
-    match User::lookup_user_by_credentials(
-        login.username.clone(),
-        login.password.clone(),
-        app_state,
-    ) {
-        Some(user) => {
-            cookies.add_private(Cookie::new("user_id", user.id.to_string()));
-            Ok(Redirect::to(uri!(index)))
-        }
-        None => Err(Flash::error(
-            Redirect::to(uri!(login_page)),
-            "Invalid username/password.",
-        )),
+    let error_msg = "Incorrect username or password";
+    match User::lookup_user_by_credentials(login.username.clone(), app_state) {
+        Some(user) => match scrypt_check(login.password.0, &user.password_digest) {
+            Ok(()) => {
+                cookies.add_private(Cookie::new("user_id", user.id.to_string()));
+                Ok(Redirect::to(uri!(index)))
+            }
+            Err(_) => Err(Flash::error(Redirect::to(uri!(login_page)), error_msg)),
+        },
+        None => Err(Flash::error(Redirect::to(uri!(login_page)), error_msg)),
     }
 }
 
@@ -142,26 +240,32 @@ fn register(
     app_state: State<AppState>,
 ) -> Result<Flash<Redirect>, Flash<Redirect>> {
     // TODO: Guard against attempts to register existing usernames or emails with a RequestGuard
-    let user_id = uuid::Uuid::new_v4();
     let error_msg = "An error occurred on our end while trying to sign you up. Please try again!";
+    let username = registration.username.clone();
+    let mut user_id: uuid::Uuid = uuid::Uuid::default();
     match app_state.users.lock() {
         Ok(mut users) => {
-            let new_user = User {
-                id: user_id,
-                username: registration.username.clone(),
-                email: registration.email.clone(),
-                preferred_name: registration.preferred_name.clone(),
-                password: registration.password.clone(), // TODO: Implement password hashing here
-            };
-            if users.contains(&new_user) {
-                return Err(Flash::error(
-                    Redirect::to(uri!(registration_page)),
-                    error_msg,
-                ));
-            }
-            match users.insert(new_user) {
-                true => (),
-                false => {
+            let new_user = User::try_from(&mut registration.into_inner());
+            match new_user {
+                Ok(new_user) => {
+                    if users.contains(&new_user) {
+                        return Err(Flash::error(
+                            Redirect::to(uri!(registration_page)),
+                            error_msg,
+                        ));
+                    }
+                    user_id = new_user.id;
+                    match users.insert(new_user) {
+                        true => (),
+                        false => {
+                            return Err(Flash::error(
+                                Redirect::to(uri!(registration_page)),
+                                error_msg,
+                            ))
+                        }
+                    }
+                }
+                Err(_) => {
                     return Err(Flash::error(
                         Redirect::to(uri!(registration_page)),
                         error_msg,
@@ -181,7 +285,7 @@ fn register(
     cookies.add_private(Cookie::new("user_id", user_id.to_string()));
     Ok(Flash::success(
         Redirect::to(uri!(index)),
-        format!("Successfully registered as {}", registration.username),
+        format!("Successfully registered as {}", username),
     ))
 }
 
