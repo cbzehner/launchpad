cac6530d10e8eb81af85b0a52e079d8f5fc243ff
Chris Zehner <cbzehner@gmail.com>
2020-09-18 08:25:47 -0500

    docs: remove user request guard design comment

--- server/src/models/user.rs
+++ server/src/models/user.rs
@@ -27,14 +27,6 @@ impl User {
     }
 }
 
-// Note: I'm actually reconsidering this TODO because this creates the need to create a blacklist or whitelist
-// of all currently active (or blacklisted) sessions. Right now I'm leaning towards the approach of "fat" sessions
-// that store most of the information in private cookies on the user's device and try to minimize the amount of trips
-// to the database ("chatty app" syndrome).
-// In the future it may make sense to store a bloom filter or more advanced data structure with just session ids that
-// can be used for server-side confirmation of the sessions, but I'm hoping to keep the database out of it.
-// TODO (api ergonomics): Write a request guard for valid sessions. Otherwise redirect to the login screen.
-
 impl<'a, 'r> FromRequest<'a, 'r> for User {
     type Error = !;
 
